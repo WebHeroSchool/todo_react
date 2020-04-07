fc52b9cc6b04fce1807a4ece009b7e3885545f29
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2020-04-07 17:24:15 +0300

    react component added

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const element = (<div>
-  My element
+  <p style ={{
+  textAlign: 'center',
+  fontSize: 36,
+  color: 'pink',
+  }}>My App</p>
 </div>);
 
-export { element };
+const ItemList = () => (
+  <ul style ={{
+    fontSize: 24,
+  }}>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
+);
+
+const App = () => (<div>
+  {element}
+  <ItemList />
+</div>);
+
+export default App;
