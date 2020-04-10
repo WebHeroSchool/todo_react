294422614d7053d1506f5cc94861fb9905ee5d9c
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2020-04-10 13:57:19 +0300

    css modules added

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
-import './App.css';
+import styles from './App.module.css';
 
 const App = () => {
   const items = [
@@ -13,8 +13,8 @@ const App = () => {
   ];
 
   return (
-  <div className="wrap">
-    <h1 className="title">Важные дела:</h1>
+  <div className={styles.wrap}>
+    <h1 className={styles.title}>Важные дела:</h1>
     <InputItem />
     <ItemList items={items} />
     <Footer count={items.length} />
