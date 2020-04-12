88df2bb25ff802cc7884695f2652defd82a758f7
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2020-04-12 17:19:27 +0300

    material ui added

--- todo/src/components/InputItem/InputItem.js
+++ todo/src/components/InputItem/InputItem.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<p>InputItem</p>);
+const InputItem = () => (<div>
+    <TextField 
+    label="Добавить задание"
+    id="outlined-margin-dense"
+    margin="dense"
+    variant="outlined"
+    />
+</div>);
 
 export default InputItem;
